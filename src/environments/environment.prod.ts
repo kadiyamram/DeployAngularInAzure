68ea63883d83b16fd582a1b8e81a7e7ae71a0709
Venkata Ramana Kadiyam <kadiyamramana@gmail.com>
2022-10-31 23:01:07 +0800

    Code Commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  application:
+  {
+    name: 'angular-ssr',
+    version: 'Angular 14.2.8',
+    bootstrap: 'Bootstrap 5.2.2',
+    fontawesome: 'Font Awesome 6.2.0',
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  application:
+  {
+    name: 'angular-ssr',
+    version: 'Angular 14.2.8',
+    bootstrap: 'Bootstrap 5.2.2',
+    fontawesome: 'Font Awesome 6.2.0',
+  }
+};
